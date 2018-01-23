7bda3ed236fbbd7272dd46ddd81f1f1c3c70da62
Nick Lewis <nicholas.lewis1@marist.edu>
2018-01-23 15:10:01 -0500

    added measurement converter program to cmpt120Lewis

---
+++ measurementconverter.py
@@ -0,0 +1,12 @@
+# Introduction to Programming
+# Author: Nicholas Lewis
+# Date: 1/23/18
+
+# measurementconverter.py
+# A program that converts inches to centimeters
+
+def measurementconverter():
+    inches  = eval(input("enter number of inches:"))
+    centimeters = 2.54 * inches
+    print("number of centimers is", centimeters)
+measurementconverter()
