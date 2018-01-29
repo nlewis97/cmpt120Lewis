0e0a8360bf19ed1d297ed7272ca9b58d5b1fbf90
Nick Lewis <nicholas.lewis1@marist.edu>
2018-01-29 17:41:01 -0500

    Solution to Curiosity rover exercise.

---
+++ rover.py
@@ -0,0 +1,13 @@
+# Introduction to Programming
+# Author: Nicholas Lewis
+#Date: 1/29/18
+
+
+# Curiosityroverexercise.py
+# A program that calculates how long it will take a photo from Curiotsity to reach NASA from 34000000 miles away.
+
+def distancecalculator():
+    miles = 34000000
+    time = miles/186000
+    print("It will take", time, "seconds for the picture to reach NASA when it is 34000000")
+distancecalculator()
