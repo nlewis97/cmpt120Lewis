d2c32f7507c8a718b1cf76c64a49268a6e5d810f
Nick Lewis <nicholas.lewis1@marist.edu>
2018-01-29 19:48:09 -0500

    solution to evens loop exercise.

---
+++ evens.py
@@ -0,0 +1,12 @@
+# Introduction to Programming
+# Author: Nicholas Lewis
+# Date: 1/29/18
+
+# evens.py
+# a program that lists even numbers from 2 - 20.
+
+for i in range(2,21,2):
+    print(i)
+    
+    
+    
