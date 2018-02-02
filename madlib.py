36449f48fdc55b3fb8245fc8812993a2c3acfd42
Nick Lewis <nicholas.lewis1@marist.edu>
2018-02-02 17:03:19 -0500

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,17 @@
+# Introduction to Programming
+# Author: Nicholas Lewis
+# Date: 2/2/18
+
+# madlib.py
+# A program that functions as a madlib
+
+def madlib():
+    name = input("enter a name:")
+    verb = input("enter a verb:")
+    adjective = input("enter an adjective:")
+    street = input("enter a street name:")
+    print(name, "started to", verb, "when the", adjective, "police officer pulled him over on", street)
+
+madlib()
+
+                      
