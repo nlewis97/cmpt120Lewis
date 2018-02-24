7dc147b39bfbae0e5f8c73b652cd179b7673e994
Nick Lewis <nicholas.lewis1@marist.edu>
2018-02-23 20:54:34 -0500

    added the credentials file to the repositroy part one of lab complete.

--- credentials.py
+++ credentials.py
@@ -2,27 +2,20 @@
 # Lab #5 – Working with Strings and Functions
 # Author: Nicholas Lewis
 # Created: 2018-02-23
-def firstName():
+def main():
     first = input("Enter your first name: ")
-    return(first)
-def lastName():
     last = input("Enter your last name: ")
-    return(last)
-def uname():
-    for i in range(5):
-        firstN=firstName();
-        lastN=lastName();
-        uname = firstN + "." + lastN
-        print("Your new usenrame is", uname.lower())
+    uname = first +"."+ last
+    print("Your new usenrame is", uname)
+    passwd = input("Create a new password: ")
+    if len(passwd) >= 8:
+        print("The force is strong in this one…")
+        print("Account configured. Your new email address is", uname + "@marist.edu")
+    elif len(passwd) <8:
+        print("Fool of a Took! That password is feeble! Create one that is at least 8 characters.")
         passwd = input("Create a new password: ")
-        if len(passwd) >= 8:
-            print("The force is strong in this one…")
-            print("Account configured. Your new email address is", uname + "@marist.edu")
-        elif len(passwd) <8:
-            print("Fool of a Took! That password is feeble! It is encouraged that you make a password at least 8 characters long.")
-            passwd = input("Create a new password: ")
-            if len(passwd) >=8:
+        if len(passwd) >=8:
                 print ("This password is worthy enough.")
                 print("Account configured. Your new email address is", uname + "@marist.edu")
    
-uname()
+main()
