0ed9c5a69ac9a02fe75401419bd9d2e16c77c0af
Nick Lewis <nicholas.lewis1@marist.edu>
2018-03-08 19:06:57 -0500

    Changes made to improve calculator functionality.  Buttons are added without functionality including the square root, square, percent, 1/x. Memory function buttons were also added without proper function.  Chagnes were made to display to accomadate the inclusion of all button and improve appearance.

--- CalculatorProject.py
+++ CalculatorProject.py
@@ -18,39 +18,37 @@ from button import Button
 class Calculator:
     def __init__(self):
         win = GraphWin("calculator")
-        win.setCoords(0,0,6,7)
+        win.setCoords(0,0,11,14.5)   
         win.setBackground("black")
         self.win = win
         self.__createButtons()
         self.__createDisplay()
-
-       
-
- 
-
+      
     def __createButtons(self):
-        bSpecs = [(2,1,'0'), (3,1,'.'),
-                  (1,2,'1'), (2,2,'2'), (3,2,'3'), (4,2,'+'), (5,2,'-'),
-                  (1,3,'4'), (2,3,'5'), (3,3,'6'), (4,3,'*'), (5,3,'/'),
-                  (1,4,'7'), (2,4,'8'), (3,4,'9'), (4,4,'<-'),(5,4,'C')]
+        bSpecs = [(2.5,1,'0'), (4,1,'.'), (1,2.5,'1'), (2.5,2.5,'2'), (4,2.5,'3'),
+                   (5.5,2.5,'+'), (7,2.5,'-'), (1,4,'4'), (2.5,4,'5'), (4,4,'6'),
+                   (5.5,4,'*'), (7,4,'/'), (1,5.5,'7'), (2.5,5.5,'8'), (4,5.5,'9'),
+                   (5.5,5.5,'<-'),(7,5.5,'C'),(1,1,'OFF'), (8.5,1,'1/x'), (8.5,2.5,'%'),
+                   (8.5,5.5,'sqrt'), (8.5,4,'x**2'), (2.5,10,'M+'), (4,10,'MS'),
+                   (5.5,10,'MR'), (1,10,'MC')]
         self.buttons = []
+        
         for (cx,cy,label) in bSpecs:
-            self.buttons.append(Button(self.win,Point(cx,cy),.75,.75,label))
-        self.buttons.append(Button(self.win, Point(4.5,1), 1.75, .75, "="))
-        for b in self.buttons:
-            b.activate()
+            self.buttons.append(Button(self.win,Point(cx,cy),1.5,1.25,label))
+        self.buttons.append(Button(self.win,Point(6.25,1),3,1.25,"="))
+        for b in self.buttons:  b.activate()
 
  
 
     def __createDisplay(self):
-        bg = Rectangle(Point(.5,5.5), Point(5.5,6.5))
+        bg = Rectangle(Point(.5,12), Point(8.5,13))
         bg.setFill('pink')
         bg.draw(self.win)
-        text = Text(Point(3,6), "")
+        text = Text(Point(4.5,12.5), "")
         text.draw(self.win)
         text.setFace("courier")
         text.setStyle("bold")
-        text.setSize(22)
+        text.setSize(10)
         self.display = text
 
 
