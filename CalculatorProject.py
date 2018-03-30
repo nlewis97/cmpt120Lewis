fd9f0c6917a1b52068e09243086a4be01c9e5999
Nick Lewis <nicholas.lewis1@marist.edu>
2018-03-29 23:41:12 -0400

    Sci button added.  When sci button is clicked new functions are displayed.  Need to make these buttons conduct operations.

--- CalculatorProject.py
+++ CalculatorProject.py
@@ -12,7 +12,7 @@
 
 from graphics import *
 
-# I could not find the button.py file in the directory
+
 from button import Button
 
  
@@ -33,7 +33,7 @@ class Calculator:
                    (5.5,4,'*'), (7,4,'/'), (1,5.5,'7'), (2.5,5.5,'8'), (4,5.5,'9'),
                    (5.5,5.5,'<-'),(7,5.5,'C'), (8.5,1,'1/x'), (8.5,2.5,'%'),
                    (8.5,5.5,'sqrt'), (8.5,4,'x**2'), (2.5,10,'M+'), (4,10,'MS'),
-                   (5.5,10,'MR'), (1,10,'MC')]
+                   (5.5,10,'MR'), (1,10,'MC'), (7.5,10,'sci')]
         self.buttons = []
         
         for (cx,cy,label) in bSpecs:
@@ -41,7 +41,21 @@ class Calculator:
         self.buttons.append(Button(self.win,Point(6.25,1),3,1.25,"="))
         for b in self.buttons:  b.activate()
 
- 
+    def __SCI(self):
+        bSpecs = [(2.5,1,'0'), (4,1,'.'), (1,2.5,'1'), (2.5,2.5,'2'), (4,2.5,'3'),
+                   (5.5,2.5,'ln'), (7,2.5,'log'), (1,4,'4'), (2.5,4,'5'), (4,4,'6'),
+                   (5.5,4,''), (7,4,'/'), (1,5.5,'7'), (2.5,5.5,'8'), (4,5.5,'9'),
+                   (5.5,5.5,'<-'),(7,5.5,'C'), (8.5,1,'x**y'), (8.5,2.5,'atan'),
+                   (8.5,5.5,'asin'), (8.5,4,'acos'), (2.5,10,'10**x'), (4,10,'sin'),
+                   (5.5,10,'cos'), (1,10,'tan')]
+        self.buttons = []
+
+        for (cx,cy,label) in bSpecs:
+            self.buttons.append(Button(self.win,Point(cx,cy),1.5,1.25,label))
+        self.buttons.append(Button(self.win,Point(6.25,1),3,1.25,"="))
+        for b in self.buttons:  b.activate()
+
+
 
     def __createDisplay(self):
         bg = Rectangle(Point(.5,12), Point(8.5,13))
@@ -125,6 +139,10 @@ class Calculator:
         elif key == 'sqrt':
             result = (eval(text))**0.5
             self.display.setText(str(result))
+
+        elif key == 'sci':
+            self.__SCI()
+            
  
 
     def run(self):
