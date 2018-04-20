05729284cced8140bf9ead5a757798160166115d
Nick Lewis <nicholas.lewis1@marist.edu>
2018-04-20 19:17:08 -0400

    Final version of robot store program added to repository.

--- RobotStore.py
+++ RobotStore.py
@@ -1,36 +1,66 @@
-productNames = [ "Ultrasonic range finder"
- , "Servo motor"
- , "Servo controller"
- , "Microcontroller Board"
- , "Laser range finder"
- , "Lithium polymer battery"
- ]
-productPrices = [ 2.50, 14.99, 44.95, 34.95, 149.99, 8.99 ]
-productQuantities = [ 4, 10, 5, 7, 2, 8 ]
+# Introduction to Programming 
+# Author: Nicholas Lewis
+# Date: 4/20/2018
+# RobotStore.py
+
+# make products into a class
+# changed name from products to availProducts to avoide repitition
+
+class Product:
+
+    def __init__  (self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+ 
+
+availProducts = [Product("Ultrasonic range finder", 2.50, 4),
+            Product("Servo motor", 14.99, 10),
+            Product("Servo controller", 44.95, 5),
+            Product("Microcontroller Board", 34.95, 7),
+            Product("Laser range finder", 149.99, 2),
+            Product("Lithium polymer battery", 8.99, 8)
+
+            ]
+
+ 
+
 def printStock():
-     print()
-     print("Available Products")
-     print("------------------")
-     for i in range(0,len(productNames)):
-            if productQuantities[i] > 0:
-                 print(str(i)+")",productNames[i], "$", productPrices[i])
-            print()
+
+    print()
+    print("Available Products")
+    print("------------------")
+    for i in range(0,len(availProducts)):
+        if availProducts[i].quantity > 0:
+            print(str(i)+")",availProducts[i].name, "$", availProducts[i].price)
+    print()
+
+   
+
 def main():
+
     cash = float(input("How much money do you have? $"))
     while cash > 0:
-         printStock()
-         vals = input("Enter product ID and quantity you wish to buy: ").split(" ")
-         if vals[0] == "quit": break
-         prodId = int(vals[0])
-         count = int(vals[1])
-    if productQuantities[prodId] >= count:
-         if cash >= productPrices[prodId]:
-             productQuantities[prodId] -= count
-             cash -= productPrices[prodId] * count
-             print("You purchased", count, productNames[prodId]+".")
-             print("You have $", "{0:.2f}".format(cash), "remaining.")
-         else:
-             print("Sorry, you cannot afford that product.")
-    else:
-        print("Sorry, we are sold out of", productNames[prodId])
+        printStock()
+        vals = input("Enter product ID and quantity you wish to buy: ").split(" ")
+        if vals[0] == "quit":break
+        prodId = int(vals[0])
+        count = int(vals[1])
+        if availProducts[prodId].quantity >= count:
+            if cash >= availProducts[prodId].price:
+                availProducts[prodId].quantity -= count
+                cash -= availProducts[prodId].price * count
+                print("You purchased", count, availProducts[prodId].name+".")
+                print("You have $", "{0:.2f}".format(cash), "remaining.")
+
+            else:
+
+                print("Sorry, you cannot afford that product.")
+
+        else:
+
+            print("Sorry, we are sold out of", availProducts[prodId].name)
+
 main()
+
